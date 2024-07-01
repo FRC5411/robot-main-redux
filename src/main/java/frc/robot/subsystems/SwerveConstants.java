6cd4238e6c14291d7b014ddc3162b7881b0a0ec9
Aryan Kammili <aryan.kammili@gmail.com>
2024-07-01 13:35:13 -0500

    Measurement changes based off CAD

--- src/main/java/frc/robot/subsystems/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/SwerveConstants.java
@@ -12,21 +12,22 @@ public class SwerveConstants{
   // TODO: Find out these measurements from zesty CAD team
     public static final double trackWidth = Units.inchesToMeters(22);
     public static final double wheelBase = Units.inchesToMeters(22);
-    public static final double wheelDiameter = Units.inchesToMeters(3.95);
+    public static final double wheelDiameter = Units.inchesToMeters(3.95); // 3.8897
 
     public static final double wheelCircumference = wheelDiameter * Math.PI;
 
     public static final int pigeonID = 10;
     
     //TODO: Find Gear Ratios
-    public static final double driveGearRatio = 0;
-    public static final double angleGearRatio = 0;
+    public static final double driveGearRatio = 6.75 / 1.0; // L2 Ratio
+    public static final double angleGearRatio = 150.0 / 7.0;
 
     public static final double driveConversionPositionFactor = (wheelDiameter * Math.PI) / driveGearRatio;
     public static final double driveConversionVelocityFactor = driveConversionPositionFactor / 60.0;
     public static final double angleConversionFactor = 360.0 / angleGearRatio;
 
     // TODO: Find accurate max linear speed as this is a rough estimate
+    //Linear Speed (meters /sec) = motor speed (RPM) / gear ratio * pi * wheel diameter (meters) / 60
     public static final int maxLinearSpeed  = 5;
 
     public static final int driftRate = 1;
