4768f34183c1e13107d8b49746f4d7a01fb67a0b
Aryan Kammili <aryan.kammili@gmail.com>
2024-07-14 15:01:25 -0500

    Swithced to krakens

--- src/main/java/frc/robot/shooter/Pivot.java
+++ src/main/java/frc/robot/shooter/Pivot.java
@@ -1,33 +1,47 @@
 package frc.robot.shooter;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.proto.ArmFeedforwardProto;
-import edu.wpi.first.wpilibj.DutyCycle;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pivot extends SubsystemBase{
 
-    private CANSparkMax pivotMotor;
-    private SparkPIDController pivotController;
+    private TalonFX rightPivotMotor;
+    private TalonFX leftPivotMotor;
     private DutyCycleEncoder pivotEncoder;
-    private ArmFeedforward pivotFF;
+    private MotionMagic
 
     public Pivot(){
-        pivotMotor = new CANSparkMax(ShooterConstants.pivotConstants.kPivotPort, MotorType.kBrushless);
+        rightPivotMotor = new TalonFX(ShooterConstants.pivotConstants.kRightPivotPort);
+        leftPivotMotor = new TalonFX(ShooterConstants.pivotConstants.kLeftPivotPort);
         pivotEncoder = new DutyCycleEncoder(ShooterConstants.pivotConstants.kAbsoulteEncoderPort);
+
         config();
     }
 
     private void config(){
-        
+        // Factory reset the krakens 
+        var reset = new TalonFXConfiguration();
+
+        rightPivotMotor.getConfigurator().apply(reset);
+        leftPivotMotor.getConfigurator().apply(reset);
+
+        rightPivotMotor.getConfigurator().apply(ShooterConstants.configs.rightConfig);
+        leftPivotMotor.getConfigurator().apply(ShooterConstants.configs.leftConfig);
     }
 
     public void setPivotPosistion(double demand){
+        rightPivotMotor.setControl(new PositionDutyCycle(demand));
+        // This allows us to make the leftPivotMotor do whatever the right pivot motor does
+        // the pivot motors were already set up to follow the same direction no need to flip
+        leftPivotMotor.setControl(new Follower(ShooterConstants.pivotConstants.kLeftPivotPort, false));
     } 
 
     public double getPivotPosistion(){
--- src/main/java/frc/robot/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/shooter/ShooterConstants.java
@@ -1,21 +1,35 @@
 package frc.robot.shooter;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.util.FeedForwardConstants;
 import frc.robot.util.PIDConstants;
 import frc.robot.util.ShooterPosistion;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterConstants{
 
     public class pivotConstants{
-        public static final int kPivotPort = 0;
+        public static final int kRightPivotPort = 0;
+        public static final int kLeftPivotPort = 0;
         public static final double kPivotGearRatio = 1.0/1.0;
         public static final double kPivotPosistionConversionFactor = 1.0/1.0;
 
         public static final int kAbsoulteEncoderPort = 0;
 
-        public static final PIDConstants pivotControllerConstants = new PIDConstants(0, 0, 0);
-        public static final FeedForwardConstants pivotControllerFFConstants = new FeedForwardConstants(0, 0, 0, 0);
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kG = 0;
+
     }
 
     public class flywheelConstants{
@@ -30,6 +44,51 @@ public class ShooterConstants{
         public static final FeedForwardConstants flywheelControlllerFFConstants = new FeedForwardConstants(0, 0, 0, 0);
 
     }
+
+    public class configs{
+        public static TalonFXConfiguration leftConfig;
+        public static TalonFXConfiguration rightConfig;
+
+        public configs(){
+            leftConfig = new TalonFXConfiguration();
+            rightConfig = new TalonFXConfiguration();
+
+            leftConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            leftConfig.CurrentLimits.StatorCurrentLimit = 60;
+            leftConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            leftConfig.CurrentLimits.SupplyCurrentLimit = 60;
+            leftConfig.Voltage.PeakForwardVoltage = 12.0;
+            leftConfig.Voltage.PeakReverseVoltage = -12.0;
+            leftConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+            leftConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+            // TODO : Check this
+            leftConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+            leftConfig.Feedback.FeedbackRemoteSensorID = (ShooterConstants.pivotConstants.kAbsoulteEncoderPort);
+
+            leftConfig.Slot0.kP = ShooterConstants.pivotConstants.kP;
+            leftConfig.Slot0.kG = ShooterConstants.pivotConstants.kG;
+            leftConfig.Slot0.kD = ShooterConstants.pivotConstants.kD;
+            leftConfig.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+            
+            rightConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+            rightConfig.CurrentLimits.StatorCurrentLimit = 60;
+            rightConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+            rightConfig.CurrentLimits.SupplyCurrentLimit = 60;
+            rightConfig.Voltage.PeakForwardVoltage = 12.0;
+            rightConfig.Voltage.PeakReverseVoltage = -12.0;
+            rightConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+            rightConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            rightConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+            rightConfig.Feedback.FeedbackRemoteSensorID = (ShooterConstants.pivotConstants.kAbsoulteEncoderPort);
+    
+            rightConfig.Slot0.kP = ShooterConstants.pivotConstants.kP;
+            rightConfig.Slot0.kG = ShooterConstants.pivotConstants.kG;
+            rightConfig.Slot0.kD = ShooterConstants.pivotConstants.kD;
+            rightConfig.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+        }
+    }
+
+
 
     public static final ShooterPosistion subwooferPosistion = new ShooterPosistion(Rotation2d.fromDegrees(1), 0);
 
